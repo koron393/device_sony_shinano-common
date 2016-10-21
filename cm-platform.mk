448ad70210c593ed60b7bd4eef9e8bf9b42f75f1
Kousuke Nagatsuka <koron393@gmail.com>
2016-10-22 00:15:43 +0900

    shinano-common: shinano platform CM hardware
    *Vibrator

--- cm-platform.mk
+++ cm-platform.mk
@@ -26,6 +26,9 @@ PRODUCT_COPY_FILES += \
 # Overlay
 DEVICE_PACKAGE_OVERLAYS += device/sony/shinano-common/overlay
 
+# CM Hardware
+BOARD_HARDWARE_CLASS += device/sony/shinano-common/cmhw
+
 # ANT+ Permissions
 PRODUCT_COPY_FILES += \
     external/ant-wireless/antradio-library/com.dsi.ant.antradio_library.xml:system/etc/permissions/com.dsi.ant.antradio_library.xml
